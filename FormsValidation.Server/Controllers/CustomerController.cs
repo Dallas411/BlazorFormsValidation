10c823ee3c878164026a2b9dfe81a8222a391809
Marco Dalla Libera <marco@dallalibera.net>
2019-04-27 09:37:52 +0200

    Customization of tamplate and validation sample

---
+++ FormsValidation.Server/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using FormsValidation.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsValidation.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public ActionResult<List<Customer>> Get()
+        {
+            var customers = new List<Customer>() {
+                new Customer() { Code ="555-0", Name="Foo", Description="Foo Desc", Active=true, NextVisitDate=DateTime.Now.AddDays(3)},
+                new Customer() { Code ="555-1", Name="Foo-1", Description="Foo Desc-1", Active=true, NextVisitDate=DateTime.Now.AddMonths(1)},
+                new Customer() { Code ="555-2", Name="Foo-2", Description="Foo Desc-2", Active=true, NextVisitDate=DateTime.Now.AddDays(5)},
+            };
+
+            return Ok(customers);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            if (ModelState.IsValid)
+                return StatusCode(201);
+                //return Created(Url.RouteUrl(customer.Id), customer.Id);
+            else
+                return BadRequest();
+        }
+    }
+}
---
+++ FormsValidation.Shared/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FormsValidation.Shared
+{
+    public class Customer
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(8,ErrorMessage = "Code invalid length: range from 0 to 8 characters.")]
+        public string Code { get; set; }
+
+        [Required]
+        [StringLength(16, ErrorMessage = "Name invalid length: range from 0 to 8 characters.")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Rating out of range, from 1 to 10 is the valid range.")]
+        public int Rating { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true",
+            ErrorMessage = "Active not valid value.")]
+        public bool Active { get; set; }
+
+        [Required]
+        public DateTime NextVisitDate { get; set; }
+    }
+}
